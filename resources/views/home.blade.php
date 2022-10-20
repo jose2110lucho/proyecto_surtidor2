6b4e678e56c2647ddd97a42f6a46aa8653e86787
julio-gonzales <juliogonzales1302@gmail.com>
2022-10-20 17:12:01 -0400

    cambios login julio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,21 @@
+<<<<<<< Updated upstream
 @extends('layouts/master')
 
 @section('content_header')
     
 @stop
+=======
+@extends('layouts.app')
+>>>>>>> Stashed changes
 
 @section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
 
+<<<<<<< Updated upstream
 
 @stop
 
@@ -18,3 +28,19 @@
 @stop
 
 @section('plugins.Sweetalert2', true);
+=======
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+>>>>>>> Stashed changes
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmpleadoController;
 use App\Http\Controllers\PremioController;
 use App\Http\Controllers\ProductoController;
+use Illuminate\Support\Facades\Auth;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,33 +18,22 @@ use App\Http\Controllers\ProductoController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Auth::routes();
 Route::get('/', function () {
-    return view('home');
+    return view('layouts/master');
 });
 
-//Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Auth::routes();
 
 
-Route::resource('empleado',EmpleadoController::class);
 
-Route::resource('producto',ProductoController::class);
+Route::resource('empleado',EmpleadoController::class)->middleware('auth');
 
-//-----------------CLIENTES-----------------//
+Route::resource('producto',ProductoController::class)->middleware('auth');
 
 Route::resource('clientes', ClienteController::class);
-Route::get('clientes/{cliente}/canjear', [ClienteController::class, 'canjeo'])->name('clientes.canjeo');
-Route::post('clientes/{cliente}/canjear', [ClienteController::class, 'canjear'])->name('clientes.canjear');
-Route::put('clientes/{cliente}/premios/{premio}', [ClienteController::class, 'destroyPremio'])->name('clientes.destroyPremio');
-
-//-----------------CLIENTES-----------------//
 
 Route::resource('tanques', TanqueController::class);
 
-Route::put('tanques/{tanque}/recargar', [TanqueController::class, 'recargar'])->name('tanques.recargar');
-
-Route::put('tanques/{tanque}/llenar', [TanqueController::class, 'llenar'])->name('tanques.llenar');
 
 Route::resource('premios', PremioController::class);
